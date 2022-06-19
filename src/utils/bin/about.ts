9a944c7d549e24bf28d1a8b5f08f3c28f3f7665b
Sam Thacker <me@samthacker.me>
2022-06-19 00:26:51 -0400

    Initial changes from fork

--- src/utils/bin/about.ts
+++ src/utils/bin/about.ts
@@ -5,3 +5,9 @@ export const about = async (args: string[]): Promise<string> => {
 
   return bio;
 };
+
+export const whoareyou = async (args: string[]): Promise<string> => {
+  const bio = await getBio();
+
+  return bio;
+};
--- src/utils/bin/projects.ts
+++ src/utils/bin/projects.ts
@@ -2,12 +2,18 @@ import { getProjects } from '../../api';
 
 export const projects = async (args: string[]): Promise<string> => {
   const projects = await getProjects();
+  var projectslist;
 
-  return projects
+  projectslist = projects
     .filter((repo) => !repo.fork)
     .map(
       (repo) =>
         `${repo.name} - <a class="text-light-blue dark:text-dark-blue underline" href="${repo.html_url}" target="_blank">${repo.html_url}</a>`,
     )
     .join('\n');
+
+  return (
+    "A sampling of my GitHub projects. For my work portfolio, try 'portfolio'! \n\n" +
+    projectslist
+  );
 };
